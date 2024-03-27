5a2115d9b04e162d4e4572efbd3341b8ebb48643
ghassant29 <gass304@gmail.com>
2024-03-27 17:43:37 +0200

    fix

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -6,7 +6,7 @@ type Props = {
 const SectionHeader = ({ subtitle, title }: Props) => {
   return (
     <div className="flex items-center justify-center flex-col gap-6 text-center">
-      <h1 className=" text-[#b12625] text-primary font-bold text-4xl md:text-5xl">
+      <h1 className=" text-[#b12625] font-bold text-4xl md:text-5xl">
         {title}
       </h1>
       <h2 className="text-primary md:text-lg px-6 md:px-0">{subtitle}</h2>
